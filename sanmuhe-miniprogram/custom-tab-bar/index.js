bb64ecec0806790b0c2087c3431178c555c6f153
banjiubanjiu <2472409840@qq.com>
2026-07-25 22:03:05 +0800

    优化小程序点单与商城底栏图标

--- sanmuhe-miniprogram/custom-tab-bar/index.js
+++ sanmuhe-miniprogram/custom-tab-bar/index.js
@@ -13,8 +13,8 @@ Component({
     selected: -1,
     list: [
       { pagePath: "pages/index/index", text: "首页", icon: "/assets/icons/home-line.png", activeIcon: "/assets/icons/home-active.png" },
-      { pagePath: "pages/order/index", text: "点单", icon: "/assets/icons/cart-line.png", activeIcon: "/assets/icons/cart-active.png" },
-      { pagePath: "pages/shop/index", text: "茶品", icon: "/assets/icons/category-line.png", activeIcon: "/assets/icons/category-active.png" },
+      { pagePath: "pages/order/index", text: "点单", icon: "/assets/icons/menu-line.png", activeIcon: "/assets/icons/menu-active.png" },
+      { pagePath: "pages/shop/index", text: "商城", icon: "/assets/icons/shop-line.png", activeIcon: "/assets/icons/shop-active.png" },
       { pagePath: "pages/profile/index", text: "我的", icon: "/assets/icons/profile-line.png", activeIcon: "/assets/icons/profile-active.png" }
     ]
   },
